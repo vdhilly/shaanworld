a312392026f8828c9a94ab44471885ea7c5eaf23
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2024-01-15 15:59:57 +0100

    Localisation Item Create

--- scripts/hooks/shaanworldHooks.js
+++ scripts/hooks/shaanworldHooks.js
@@ -1,3 +1,5 @@
+// import { querySelectorAll } from "../../module/utils/utils.js"
+
 export default () => {
 
     Hooks.on('renderChatMessage', async (message, html, data) => {
@@ -6,6 +8,19 @@ export default () => {
         if($rollMessage){
             const $chatMessage = $rollMessage.closest('.chat-message')
             $chatMessage.addClass('roll-message')
+        }
+    })
+
+    Hooks.on('renderDialog', async (dialog, html, data) => {
+        console.log(dialog, html, data)
+        if(dialog.title === "Create New Item"){
+            const $html = html[0]
+
+            const itemOptions =$html.querySelectorAll('select[name="type"] option')
+            for (const itemOption of itemOptions) {
+                itemOption.textContent = game.i18n.localize('Item.' + itemOption.textContent)
+            }
+            
         }
     })
 }
